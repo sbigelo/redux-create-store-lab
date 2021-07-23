0a23888924c0c9b4df275251a216f766aee6a3fd
sbigelo <sean.bigelow1@gmail.com>
2021-07-23 12:39:00 -0400

    commit tree 48316bd3ff0f55eb642624f72d360dfeef9ce539

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 // write your createStore function here
 
+function createStore(reducer) {
+  let state;
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+  function getState() {
+    return state;
+  }
+  return{dispatch, getState}
+}
+
 function candyReducer(state = [], action) {
   switch (action.type) {
     case 'ADD_CANDY':
@@ -18,5 +30,7 @@ function render() {
   }
 };
 
+let store = createStore(candyReducer)
+store.dispatch({type: '@@INIT'})
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
